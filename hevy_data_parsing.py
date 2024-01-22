473198d5064decafada03fdcbd4f5ed5f8e4fbb0
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2024-01-22 10:11:20 +0100

    TEST TO REVERT

--- hevy_data_parsing.py
+++ hevy_data_parsing.py
@@ -1,21 +1,23 @@
 import pandas as pd
 
-# Read CSV into DataFrame
-df = pd.read_csv('workouts.csv')
+def parse_csv(data):
+    # Read CSV into DataFrame
+    df = pd.read_csv(data)
 
+    # Calculate volume (weight_kg x reps)
+    df['best_set_volume'] = df['weight_kg'] * df['reps']
+    df['total_volume'] = df['weight_kg'] * df['reps']
+    df['1rm'] = df['weight_kg'] * (1 + (df['reps'] / 30))
 
-# Calculate volume (weight_kg x reps)
-df['best_set_volume'] = df['weight_kg'] * df['reps']
-df['total_volume'] = df['weight_kg'] * df['reps']
-df['1rm'] = df['weight_kg'] * (1 + (df['reps'] / 30))
+    # Make new DF where the exercises are grouped by date, we keep the best 1RM and best volume and we sum the total volume for each exercise
+    df = df.groupby(['start_time','exercise_title']).agg({'1rm': 'max', 'best_set_volume': 'max', 'total_volume': 'sum', 'weight_kg': 'max', 'reps': 'sum'}).reset_index()
+    df = df.rename(columns={'weight_kg' : 'heaviest_weight', 'reps' : 'total_reps'})
 
-# Make new DF where the exercises are grouped by date, we keep the best 1RM and best volume and we sum the total volume for each exercise
-df = df.groupby(['start_time','exercise_title']).agg({'1rm': 'max', 'best_set_volume': 'max', 'total_volume': 'sum', 'weight_kg': 'max', 'reps': 'sum'}).reset_index()
-df = df.rename(columns={'weight_kg' : 'heaviest_weight', 'reps' : 'total_reps'})
+    df['start_time'] = pd.to_datetime(df['start_time'], format='%d %b %Y, %H:%M')
+    df['start_time'] = df['start_time'].apply(lambda x: x.timestamp()*1000)
 
-df['start_time'] = pd.to_datetime(df['start_time'], format='%d %b %Y, %H:%M')
-df['start_time'] = df['start_time'].apply(lambda x: x.timestamp()*1000)
+    # Sort df by number of occurrences of each exercise
+    df['exercise_numinstances'] = df.groupby('exercise_title')['exercise_title'].transform('count')
+    df = df.sort_values(by=['exercise_numinstances'], ascending=False)
 
-# Sort df by number of occurrences of each exercise
-df['exercise_numinstances'] = df.groupby('exercise_title')['exercise_title'].transform('count')
-df = df.sort_values(by=['exercise_numinstances'], ascending=False)
+    return df
--- hevy_to_graph.py
+++ hevy_to_graph.py
@@ -1,15 +1,25 @@
-from hevy_data_parsing import df
-
-from nicegui import ui
-from nicegui.events import ValueChangeEventArguments
+from hevy_data_parsing import parse_csv
+from io import StringIO
+from nicegui import ui, events
 
 ui.page_title('Hevy Graph Maker')
 ui.label('Hevy graph demo')
 
-list_of_exercises = df['exercise_title'].unique().tolist()
-selected_exercise = list_of_exercises[0]
-toggled_metric = 'heaviest_weight'
+is_uploaded = False
+v = ui.checkbox('Show upload', value=False)
 
+def handle_upload(event: events.UploadEventArguments):
+    ui.notify(f'Uploaded {event.name}')
+    text = event.content.read().decode('utf-8')
+    global df
+    df = parse_csv(StringIO(text))
+    global v, list_of_exercises, selected_exercise
+    v.value = True
+    list_of_exercises = df['exercise_title'].unique().tolist()
+    selected_exercise = list_of_exercises[0]
+    select.set_options(list_of_exercises, value=selected_exercise)
+    update_chart()
+    
 def update_chart():
     get_exercise = df[df['exercise_title'] == selected_exercise]
     get_exercise = get_exercise.sort_values(by=['start_time'], ascending=True)
@@ -18,7 +28,7 @@ def update_chart():
     chart.options['series'][0]={'name': metric_dict[toggled_metric], 'data': list(zip(times, metric)), 'marker': {'symbol': 'diamond', 'enabled': True}}
     chart.update()
 
-def selector_changed(event: ValueChangeEventArguments):
+def selector_changed(event: events.ValueChangeEventArguments):
     name = type(event.sender).__name__
     if name == 'Select':
         global selected_exercise
@@ -28,18 +38,23 @@ def selector_changed(event: ValueChangeEventArguments):
         toggled_metric = event.value
     update_chart()
     
+
+list_of_exercises = ['No exercises uploaded']
+selected_exercise = list_of_exercises[0]
+toggled_metric = 'heaviest_weight'
 metric_dict = {'heaviest_weight':'Heaviest Weight','1rm':'One Rep Max', 'best_set_volume':'Best Set Volume', 'total_volume':'Session Volume', 'total_reps':'Total Reps'}
 
-ui.select(list_of_exercises, value=list_of_exercises[0], on_change=selector_changed)
-ui.toggle(metric_dict, value='heaviest_weight', on_change=selector_changed)
+upload = ui.upload(on_upload=handle_upload, on_rejected=lambda: ui.notify('Rejected!'), max_file_size=5_000_000).props('accept=.csv').classes('max-w-full')
 
-chart = ui.highchart({
-        'title': False,
-        'xAxis': {'type': 'datetime'},
-        'yAxis': {'labels': {'format': '{value} kg'}},
-        'series': [{}],
-    })
+with ui.column().bind_visibility_from(v, 'value'):
+    select = ui.select(list_of_exercises, value=list_of_exercises[0], on_change=selector_changed)
+    ui.toggle(metric_dict, value='heaviest_weight', on_change=selector_changed)
 
-update_chart()
+    chart = ui.highchart({
+            'title': False,
+            'xAxis': {'type': 'datetime'},
+            'yAxis': {'labels': {'format': '{value} kg'}},
+            'series': [{}],
+        })
 
 ui.run()
